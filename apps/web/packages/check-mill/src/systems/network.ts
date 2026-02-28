84fdfde0f73b88898c2d584a734051316ac4ef02
fliseno1k <fliseno1k@gmail.com>
2026-02-28 19:05:03 +0200

    feat: add turborepo

--- apps/web/packages/check-mill/src/systems/network.ts
+++ apps/web/packages/check-mill/src/systems/network.ts
@@ -1,9 +1,22 @@
-import type { AppRef, AppSystemInstance } from "../components";
-import { noop } from "../core";
+import { Phases, type AppRef, type AppSystemInstance } from "../components";
+import type { Disposable } from "../core";
+
+import { Socket } from "phoenix";
 
 export function NetworkSystem(_appRef: AppRef): AppSystemInstance {
+  function init(): Disposable {
+    const socket = new Socket("ws://localhost:4000/view/websocket?vsn=2.0.0", {});
+
+    socket.connect();
+
+    return () => socket.disconnect();
+  }
+
   return {
-    init: () => noop,
-    logic: {},
+    init,
+    logic: {
+      [Phases.IO]: [],
+      [Phases.Cleanup]: [],
+    },
   };
 }
